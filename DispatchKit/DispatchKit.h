991105cc1953aa8fe40c020b7cb3d6cfbf9d33e7
John Rommel Estropia <john.rommel.estropia@gmail.com>
2014-08-24 20:05:07 +0900

    Initial Commit

---
+++ DispatchKit/DispatchKit.h
@@ -0,0 +1,19 @@
+//
+//  DispatchKit.h
+//  DispatchKit
+//
+//  Created by John Rommel Estropia on 2014/08/24.
+//  Copyright (c) 2014 John Rommel Estropia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DispatchKit.
+FOUNDATION_EXPORT double DispatchKitVersionNumber;
+
+//! Project version string for DispatchKit.
+FOUNDATION_EXPORT const unsigned char DispatchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DispatchKit/PublicHeader.h>
+
+
